481e0aaa48944d231fea9eb6001ea943afd1dc9f
azizs <aziz0329@yahoo.com>
2019-04-22 23:33:19 +0700

    add layout

--- app/src/main/java/com/example/sigadmin/layouts/InputNewSubField.kt
+++ app/src/main/java/com/example/sigadmin/layouts/InputNewSubField.kt
@@ -3,7 +3,9 @@ package com.example.sigadmin.layouts
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.Button
+import android.widget.Spinner
 import com.example.sigadmin.R
 
 class InputNewSubField : AppCompatActivity() {
@@ -13,10 +15,15 @@ class InputNewSubField : AppCompatActivity() {
         setContentView(R.layout.input_new_sub_field)
 
         val saveNewSubFieldBtn = findViewById<Button>(R.id.saveNewSubFieldButton)
+        val dropJenis = findViewById<Spinner>(R.id.spnrJenisLapangan)
 
         saveNewSubFieldBtn.setOnClickListener {
-            val intent = Intent (this, LoginActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        val jenisLap = arrayOf("Vinyl", "Sintetis")
+        val arrayAdp = ArrayAdapter(this@InputNewSubField,R.layout.input_new_sub_field,jenisLap)
+        dropJenis.adapter = arrayAdp
     }
 }
