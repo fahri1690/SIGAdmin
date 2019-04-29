16a33ad3fc122c4656111a1261e142e7811db825
Fahri Renngur <fahrirenngur@outlook.com>
2019-04-30 01:26:56 +0700

    modify register screen

--- app/src/main/java/com/example/sigadmin/layouts/RegisterActivity.kt
+++ app/src/main/java/com/example/sigadmin/layouts/RegisterActivity.kt
@@ -1,16 +1,24 @@
 package com.example.sigadmin.layouts
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
 import com.example.sigadmin.R
 import com.example.sigadmin.models.Admin
+import com.google.firebase.FirebaseException
+import com.google.firebase.FirebaseTooManyRequestsException
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.core.Tag
+import kotlinx.android.synthetic.main.activity_field_profile.*
 import kotlinx.android.synthetic.main.register.*
+import java.util.concurrent.TimeUnit
 
 class RegisterActivity : AppCompatActivity() {
 
@@ -29,6 +37,42 @@ class RegisterActivity : AppCompatActivity() {
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
+
+        val callbacks = object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+            override fun onVerificationCompleted(credential: PhoneAuthCredential) {
+                Log.d(TAG, "onVerificationComplete:$credential")
+                signInWithPhoneAuthCredential(credential)
+            }
+
+            override fun onVerificationFailed(e: FirebaseException) {
+                Log.w(TAG, "onVerificationFailed", e)
+
+                if (e is FirebaseAuthInvalidCredentialsException){
+                    print("Kode Salah")
+                }else if (e is FirebaseTooManyRequestsException){
+                    print("Melewati batas pengiriman")
+                }
+
+                print("Silahkan ulangi")
+            }
+
+            override fun onCodeSent(verificationId: String?, token: PhoneAuthProvider.ForceResendingToken) {
+                Log.d(TAG, "onCodeSent:" + verificationId)
+
+                storedVerificationId = verificationId
+                resendToken = token
+            }
+
+        }
+
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+            phoneNumber,
+            60,
+            TimeUnit.SECONDS,
+            this,
+            callbacks
+        )
+
 
     }
 
